58f6292d89232f352909f6452b800b52953f8d24
ogcode <ogcode@local>
2026-05-05 01:13:47 +0530

    Register @fastify/helmet in app.js and update project structure

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const fastify = require('fastify');
+const helmet = require('@fastify/helmet');
 const rootRoutes = require('./routes/root');
 const birthdayRoutes = require('./routes/birthday');
 
 function buildApp(opts = {}) {
   const app = fastify(opts);
 
+  app.register(helmet);
   app.register(rootRoutes);
   app.register(birthdayRoutes);
 
