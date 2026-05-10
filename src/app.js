56ef9c37c6cadf4d5524f6a1514b4682beac6669
ogcode <ogcode@local>
2026-05-11 00:07:01 +0530

    Register Christmas routes plugin in app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const fastify = require('fastify');
 const rootRoutes = require('./routes/root');
 const birthdayRoutes = require('./routes/birthday');
+const christmasRoutes = require('./routes/christmas');
 
 function buildApp(opts = {}) {
   const app = fastify(opts);
 
   app.register(rootRoutes);
   app.register(birthdayRoutes);
+  app.register(christmasRoutes);
 
   return app;
 }
