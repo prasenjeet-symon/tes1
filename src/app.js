9ed54f0b47f7c281366af89ec452fdd816ecf9e9
ogcode <ogcode@local>
2026-05-11 17:34:41 +0530

    Register Diwali routes in app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const fastify = require('fastify');
 const rootRoutes = require('./routes/root');
 const birthdayRoutes = require('./routes/birthday');
+const diwaliRoutes = require('./routes/diwali');
 
 function buildApp(opts = {}) {
   const app = fastify(opts);
 
   app.register(rootRoutes);
   app.register(birthdayRoutes);
+  app.register(diwaliRoutes);
 
   return app;
 }
